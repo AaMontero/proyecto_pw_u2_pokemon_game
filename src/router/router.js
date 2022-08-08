13f5b660da4a6d01853eea4174ae5b8b421abe7d
Anthonny Montero <heatkdwow@gmail.com>
2022-08-08 07:35:18 -0500

    Taller23

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,21 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
-import AboutPage from '../modulos/pokemon/pages/AboutPage.vue'
-import ListPage from '../modulos/pokemon/pages/ListPage.vue'
-import PokemonPage from '../modulos/pokemon/pages/PokemonPage.vue'
-import NoPageFound from '../modulos/pokemon/pages/NoPageFound.vue'
+//import AboutPage from '../modulos/pokemon/pages/AboutPage.vue'
+//import ListPage from '../modulos/pokemon/pages/ListPage.vue'
+//import PokemonPage from '../modulos/pokemon/pages/PokemonPage.vue'
+//import NoPageFound from '../modulos/pokemon/pages/NoPageFound.vue'
 
-const routes =[
+/*const routes =[
     {path:'/', component:AboutPage},
     {path:'/list', component:ListPage},
     {path:'/pokemon', component:PokemonPage},
     {path: '/pathMatch(.*)', component:NoPageFound},
+]*/ 
+
+const routes =[
+    {path:'/', component: ()=> import (/*webpackChunkName: "AboutPage"*/'../modulos/pokemon/pages/AboutPage.vue')},
+    {path:'/list', component: ()=> import (/*webpackChunkName: "ListPage"*/ '../modulos/pokemon/pages/ListPage.vue')},
+    {path:'/pokemon', component:()=> import (/*webpackChunkName: "PokemonPage"*/ '../modulos/pokemon/pages/PokemonPage.vue')},
+    {path: '/:pathMatch(.*)', component: ()=> import ('../modulos/pokemon/pages/NoPageFound.vue')},
 ]
 const router = createRouter({
     history: createWebHashHistory(),
